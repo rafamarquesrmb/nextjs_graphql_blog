20f2d6d7958d5cac6b592d37a6a2249c7df389cc
rafamarquesrmb <rafa.rmb@gmail.com>
2022-09-14 15:09:37 -0300

    CommentsForm created

--- services/index.js
+++ services/index.js
@@ -123,3 +123,13 @@ export const getPostDetails = async (slug) => {
   const result = await request(graphqlAPI, query, { slug });
   return result.post;
 };
+export const submitComment = async (obj) => {
+  const result = await fetch("/api/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(obj),
+  });
+  return result.json();
+};
