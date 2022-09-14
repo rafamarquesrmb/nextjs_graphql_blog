1da769926a5395bb9dae29a822b13ce870c397b4
rafamarquesrmb <rafa.rmb@gmail.com>
2022-09-13 22:53:28 -0300

    Categories component created

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,16 @@
-import React, { useContext } from "react";
-
+import React, { useState, useEffect } from "react";
+import { getCategories } from "../services";
 import Link from "next/link";
 
-const categories = [
-  {
-    name: "React",
-    slug: "react",
-  },
-  {
-    name: "web development",
-    slug: "webdev",
-  },
-];
-
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
